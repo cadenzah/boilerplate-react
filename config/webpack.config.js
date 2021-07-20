802c3bae4b45921e550170b05f0874788b1edc67
cadenzah <cadenzah93@gmail.com>
2021-07-20 23:46:24 +0900

    changed indentation (2->4)
    - webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -9,91 +9,91 @@ module.exports = (env) => {
     const { MODE } = process.env;
     const isDev = MODE === 'development';
 
-  // load environment variables to use
-  const envKeys = require('./env')(env);
+    // load environment variables to use
+    const envKeys = require('./env')(env);
 
-  // file paths
-  const configPath = path.join(__dirname); // path for webpack.config.js
-  const rootPath = path.join(configPath, '..');
-  const buildPath = path.join(rootPath, 'build');
-  const srcPath = path.join(rootPath, 'src');
+    // file paths
+    const configPath = path.join(__dirname); // path for webpack.config.js
+    const rootPath = path.join(configPath, '..');
+    const buildPath = path.join(rootPath, 'build');
+    const srcPath = path.join(rootPath, 'src');
 
-  // Base Configuration Option
-  let config = {
-    entry: ["core-js/stable", "regenerator-runtime/runtime", `${srcPath}/index.js`],
-    output: {
-      publicPath: '/',
-      filename: 'js/[name].[chunkhash].js',
-      path: buildPath
-    },
-    mode: isDev ? 'development' : 'production',
-    module: {
-      rules: [
-        {
-          test: /\.(css|sass|scss)$/,
-          use: ['style-loader', 'css-loader', 'sass-loader']
+    // Base Configuration Option
+    let config = {
+        entry: ["core-js/stable", "regenerator-runtime/runtime", `${srcPath}/index.js`],
+        output: {
+            publicPath: '/',
+            filename: 'js/[name].[chunkhash].js',
+            path: buildPath,
         },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: "/node_modules",
-          use: ['babel-loader']
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HTMLWebpackPlugin({
-        template: './public/index.html',
-        minify: isDev && {
-          collapseWhitespace: true,
-          removeComments: true,
-          minifyJS: true,
+        mode: isDev ? 'development' : 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.(css|sass|scss)$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: "/node_modules",
+                    use: ['babel-loader'],
+                },
+            ],
         },
-        hash: isDev,
-      }),
-      new WebpackManifestPlugin({
-        fileName: 'manifest.json'
-      }),
-      new CopyWebpackPlugin({
-        // Inject static assets into public directory
-        // To access them in your component,
-          // put them in `public/` directory in whichever form - directories, files
-          // and use them with the url which has prefix `assets/`.
-        // See the example usage
-        patterns: [
-          {
-            from: 'public/assets',
-            to: './assets',
-            toType: 'dir',
-          },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HTMLWebpackPlugin({
+                template: './public/index.html',
+                minify: isDev && {
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    minifyJS: true,
+                },
+                hash: isDev,
+            }),
+            new WebpackManifestPlugin({
+                fileName: 'manifest.json',
+            }),
+            new CopyWebpackPlugin({
+            // Inject static assets into public directory
+            // To access them in your component,
+                // put them in `public/` directory in whichever form - directories, files
+                // and use them with the url which has prefix `assets/`.
+            // See the example usage
+                patterns: [
+                    {
+                        from: 'public/assets',
+                        to: './assets',
+                        toType: 'dir',
+                    },
+                ],
+            }),
+            new webpack.DefinePlugin(envKeys.stringified),
+            new webpack.ProvidePlugin({
+                process: 'process/browser',
+            }),
         ],
-      }),
-      new webpack.DefinePlugin(envKeys.stringified),
-      new webpack.ProvidePlugin({
-        process: 'process/browser',
-    }),
-    ],
-  };
+    };
 
-  if (MODE === 'development') {
-      // for DEVELOPMENT BUILD
-      config = {
-          devtool: 'inline-source-map',
-          devServer: {
-            contentBase: buildPath,
-            compress: true,
-            port: 3000,
-            historyApiFallback: true,
-            hot: true,
-          },
-          plugins: config.plugins.concat([
-            new webpack.HotModuleReplacementPlugin(),
-          ]),
-          ...config,
-      };
-  } else {
-      // for PRODUCTION BUILD
-      config = {
+    if (MODE === 'development') {
+        // for DEVELOPMENT BUILD
+        config = {
+            devtool: 'inline-source-map',
+            devServer: {
+                contentBase: buildPath,
+                compress: true,
+                port: 3000,
+                historyApiFallback: true,
+                hot: true,
+            },
+            plugins: config.plugins.concat([
+                new webpack.HotModuleReplacementPlugin(),
+            ]),
+            ...config,
+        };
+    } else {
+        // for PRODUCTION BUILD
+        config = {
         optimization: {
             runtimeChunk: 'single',
             splitChunks: {
@@ -101,16 +101,14 @@ module.exports = (env) => {
                     vendor: {
                         test: /[\\/]node_modules[\\/]/,
                         name: 'vendors',
-                        chunks: 'all'
+                        chunks: 'all',
                     }
-                }
-            }
+                },
+            },
         },
         ...config,
-      }
-  }
+        };
+    }
 
-  console.log(config)
-
-  return config
-}
+    return config;
+};
